81b4b3af206bba24e13bda893d6f461defcf1f67
Jack Li <jack.teng.li@gmail.com>
2019-04-15 20:30:06 -0400

    update

--- js/projects.js
+++ js/projects.js
@@ -2,11 +2,13 @@
 $('document').ready(function(){
   $('#projects').click(function(e) {
     $('#projects-menu').toggleClass('open');
+    $('body').toggleClass('no-scroll');
     e.preventDefault()
   });
 
   $('.close-button').click(function(e) {
     $('#projects-menu').removeClass('open');
+    $('body').removeClass('no-scroll');
     e.preventDefault()
   });
 });
