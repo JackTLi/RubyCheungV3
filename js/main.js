844287867b16eb2c0b569ce6729391e6956b5a9a
Jack Li <jack.teng.li@gmail.com>
2019-04-21 18:27:57 -0400

    zindexes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 $(window).scroll( function(){
   if ($(window).scrollTop() > 120) {
-    if (!$('#header').hasClass('background')) {
-      $('#header').addClass('background');
+    if (!$('.header-background').hasClass('scrolled')) {
+      $('.header-background').addClass('scrolled');
     }
   }
 
   if ($(window).scrollTop() < 80) {
-    if ($('#header').hasClass('background')) {
-      $('#header').removeClass('background');
+    if ($('.header-background').hasClass('scrolled')) {
+      $('.header-background').removeClass('scrolled');
     }
   }
 });
